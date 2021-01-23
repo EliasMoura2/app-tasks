c75d81766ce528ad93a737490b77fc7a1435fec4
Elias Moura <d.eliasmoura2@gmail.com>
2021-01-23 00:25:39 -0300

    update task

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -30,16 +30,55 @@ module.exports = {
       })
     }
   },
+  doneTask: async(req, res) => {
+    try {
+      // console.log(task)
+      // await Task.findByIdAndDelete(req.params.id)
+      // res.redirect('/')
+      const task = await Task.findById(req.params.id)
+      task.status = !task.status
+      task.save()
+      // res.send('received')
+      res.redirect('/')
+    } catch (error) {
+      res.status(500).json({
+        message: error.message
+      })
+    }
+  },
+  getUpdateTask: async (req, res) => {
+    try {
+      const task = await Task.findById(req.params.id)
+      // console.log(task)
+      res.render('edit', { task })
+    } catch (error) {
+      res.status(500).json({
+        message: error.message
+      })
+    }
+  },
+  postUpdateTask: async (req, res) => {
+    try {
+      const { title, description, status } = req.body
+      const update_values = { title, description, status }
+      const task = await Task.findByIdAndUpdate(req.params.id, update_values)
+      // console.log(task)
+      res.redirect('/')
+    } catch (error) {
+      res.status(500).json({
+        message: error.message
+      })
+    }
+  },
   deleteTask: async (req, res) => {
     try {
-      // const task = req.params.id
       // console.log(task)
       // const userDelete = await User.findById(req.params.id)
       // res.status(200).json({
         //   success: true,
         //   message: 'User delete',
         //   task
-        // })
+      // })
       await Task.findByIdAndDelete(req.params.id)
       res.redirect('/')
     } catch (error) {
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -4,6 +4,9 @@ const taskController = require('../controllers/task.controller')
 
 router.get('/', taskController.allTasks)
 router.post('/create', taskController.addTask)
+router.get('/turn/:id', taskController.doneTask)
+router.get('/update/:id', taskController.getUpdateTask)
+router.post('/update/:id', taskController.postUpdateTask)
 router.get('/delete/:id', taskController.deleteTask)
 
 module.exports = router
