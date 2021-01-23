4c334fbe3831b91c412666fedd67a9ab584b3484
Elias Moura <d.eliasmoura2@gmail.com>
2021-01-23 18:03:28 -0300

    variables de entorno database

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,29 @@
 const mongoose = require('mongoose');
 
+// mongoose
+//   .connect(process.env.MONGODB_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   })
+//     .then((db) => console.log('DB is Connected'))
+//     .catch((err) => console.error(err))
 mongoose.connection.on('open', () => console.log('DB IS CONNECTED'))
 async function connectDB({ host, port, dbname }){
-  const URI = `mongodb://${host}:${port}/${dbname}`
-  await mongoose.connect(URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+  if(process.env.NODE_ENV === 'development'){
+    const URI = `mongodb://${host}:${port}/${dbname}`
+    await mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  } else if (process.env.NODE_ENV === 'production'){
+    const URI = process.env.MONGODB_URI
+    await mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+      .then((db) => console.log(`DB is Connected ${process.env.MONGODB_URI}`))
+      .catch((err) => console.error(err))
+  }
 }
 
 module.exports = connectDB
