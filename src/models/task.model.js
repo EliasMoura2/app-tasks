9b92637a6b67993d571bac5c233bc5d3da0fcbdb
Elias Moura <d.eliasmoura2@gmail.com>
2021-03-18 07:40:11 -0300

    en los forms solo podemos usar POST y GET

--- src/models/task.model.js
+++ src/models/task.model.js
@@ -1,9 +1,15 @@
 const { Schema, model } = require('mongoose');
 
 const taskSchema = new Schema({
-  title: { type: String, required: [true, 'is required'] },
-  description: { type: String, required: [true, 'is required'] },
-  status: { type: Boolean, default: false }
+  title: { 
+    type: String,
+    required: [true, 'is required'] },
+  description: { 
+    type: String, 
+    required: [true, 'is required'] },
+  status: { 
+    type: Boolean,
+    default: false }
 }, {
   timestamps: true
 })
